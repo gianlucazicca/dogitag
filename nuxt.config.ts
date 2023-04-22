c27205eac3577c620a7fa543a80142b04f283b0d
gianlucazicca.connectoor <zicca@connectoor.com>
2023-04-22 17:43:32 +0200

    Add Tailwindcss and dir components, pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from 'path';
 export default defineNuxtConfig({
-
-})
+  alias: {
+    '@': resolve(__dirname, '/'),
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
