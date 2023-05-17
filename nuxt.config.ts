cf3db43c2cefa7ec6cecf246deaa0f65373744a5
gianlucazicca.connectoor <zicca@connectoor.com>
2023-05-17 19:28:08 +0200

    enable nuxt devtools in nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { resolve } from 'path';
 export default defineNuxtConfig({
+  modules: [
+    '@nuxt/devtools',
+  ],
+  devtools: {
+    enabled: true,
+  },
   alias: {
     '@': resolve(__dirname, '/'),
   },
